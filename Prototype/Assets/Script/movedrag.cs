1f8f57b7eb0793b3a3f5be298ff44c91ceb74656
DeJaVu <whzbrady2@hotmail.com>
2020-11-23 18:42:47 +0100

    Prototype updated

---
+++ Prototype/Assets/Script/movedrag.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movedrag : MonoBehaviour
+{
+    public string target;
+    public Camera cam;
+    public GameObject Obj=null;
+    public bool pc;
+    public float sens=0.001f;
+    public Material selected;
+    public Material normal;
+    private Touch touch;
+
+    
+    void Update()
+    {
+        if (!pc)
+        {
+            if (Input.touchCount > 0)
+            {
+                
+                touch = Input.GetTouch(0);
+                Vector3 pos = touch.position;
+                if (touch.phase == TouchPhase.Began)
+                {
+                    RaycastHit hit;
+                    Ray ray = cam.ScreenPointToRay(pos);
+
+                    if (Physics.Raycast(ray, out hit) && hit.collider.tag == target)
+                    {
+
+                        Obj = hit.collider.gameObject as GameObject;
+
+                    }
+                }
+                if (touch.phase == TouchPhase.Moved&&Obj!=null)
+                {
+                    Obj.transform.position = new Vector3(Obj.transform.position.x + touch.deltaPosition.x * sens,
+                        Obj.transform.position.y,
+                        Obj.transform.position.z + touch.deltaPosition.y * sens);
+                }
+                if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+                {
+                    Obj = null;
+                }
+            }
+        }
+        else if(pc) {
+            Debug.Log("using pc");
+            RaycastHit hit;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit) && hit.collider.tag == target)
+            {
+                Obj = hit.collider.gameObject as GameObject;
+                Obj.transform.GetComponent<Renderer>().material = selected;
+                Debug.Log(Obj.name);
+            }
+            else { Obj = null; }
+            if (Input.GetMouseButtonDown(0)&&Obj!=null)
+            {
+                Vector3 mPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -cam.transform.position.z + Obj.transform.position.z);
+                Obj.transform.position = cam.ScreenToWorldPoint(Input.mousePosition);
+            }
+           
+           
+        }
+    }
+    
+}
---
+++ Prototype/Assets/Script/puzzel1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class puzzel1 : MonoBehaviour
+{
+    public GameObject tree;
+    private int times;
+    void OnTriggerEnter(Collider collision)
+    {
+        
+        if (collision.gameObject.tag == "Dragble")
+        {
+            GameObject go = collision.gameObject as GameObject;
+            Destroy(go);
+            times++;
+            if (times==3)
+            {
+                tree.SetActive(true);
+            }
+        }
+    }
+}
