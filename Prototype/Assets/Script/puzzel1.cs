22fa32d6faf481bec1d2714f27767988cfc146fe
DeJaVu <whzbrady2@hotmail.com>
2020-11-26 11:40:55 +0100

    first Scene rework for final

--- Prototype/Assets/Script/puzzel1.cs
+++ Prototype/Assets/Script/puzzel1.cs
@@ -5,19 +5,37 @@ using UnityEngine;
 public class puzzel1 : MonoBehaviour
 {
     public GameObject tree;
+    
     private int times;
+
+  
     void OnTriggerEnter(Collider collision)
     {
         
         if (collision.gameObject.tag == "Dragble")
         {
+            Debug.Log("triggerd");
             GameObject go = collision.gameObject as GameObject;
             Destroy(go);
             times++;
-            if (times==3)
+            switch (times)
             {
-                tree.SetActive(true);
+                case 1:
+                    tree.transform.GetChild(1).gameObject.SetActive(true);
+                    break;
+                case 2:
+                    tree.transform.GetChild(2).gameObject.SetActive(true);
+                    break;
+                case 3:
+                    tree.transform.GetChild(3).gameObject.SetActive(true);
+                    break;
+                case 4:
+                    tree.transform.GetChild(4).gameObject.SetActive(true);
+                    break;
+
             }
         }
+
+
     }
 }
