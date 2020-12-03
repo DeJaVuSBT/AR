d4b6716f6b5585e7294f67747287247b6a60640b
DeJaVu <whzbrady2@hotmail.com>
2020-12-03 09:57:19 +0100

    11

--- Prototype/Assets/Script/Sainta.cs
+++ Prototype/Assets/Script/Sainta.cs
@@ -1,16 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Sainta : MonoBehaviour
 {
     public GameObject target;
+    float time = 5;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject== target)
         {
             GetComponent<AudioSource>().Stop();
-            GetComponent<Animation>().Play("Wake"); 
+            GetComponent<Animation>().Play("Wake");
+            Ending();
         }
+    }
+
+    void Ending() {
+        time -= Time.deltaTime;
+        if (time<0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        
     }
 }
